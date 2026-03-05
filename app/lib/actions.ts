480aff7e915c48e51cd0fb1c8d019764e0c8047b
Thodoris Tsironis <theogr99@gmail.com>
2026-03-05 18:11:26 +0200

    Added redirect:false to manually handle redirect

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,22 +1,29 @@
-'use server';
+"use server";
 
-import { signIn } from '@/auth';
-import { AuthError } from 'next-auth';
-import { redirect } from 'next/navigation';
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+import { redirect } from "next/navigation";
 
-export async function authenticate(prevState: string | undefined, formData: FormData) {
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
   try {
-    await signIn('credentials', formData);
+    await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirect: false,
+    });
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
+        case "CredentialsSignin":
+          return "Invalid credentials.";
         default:
-          return 'Something went wrong.';
+          return "Something went wrong.";
       }
     }
     throw error;
   }
-  redirect('/dashboard');
+  redirect("/dashboard");
 }
